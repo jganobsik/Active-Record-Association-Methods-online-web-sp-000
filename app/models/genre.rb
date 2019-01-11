ee94c6eb500306657891198ec48abcc562844d1f
Joe Ganobsik <jganobsik@gmail.com>
2019-01-11 15:42:16 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -8,7 +8,7 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    artist_item = Genre.artist 
+    song_list = self.songs  
     artist_item.length
   end
 
